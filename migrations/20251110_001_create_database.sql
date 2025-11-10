f3a914eb372f775ca7e1e6bbec13da080dacfb87
EnzoPenido <enzopoalencar01@gmail.com>
2025-11-10 16:23:01 -0300

    Criação da estrutura do projeto

---
+++ migrations/20251110_001_create_database.sql
@@ -0,0 +1,8 @@
+--Migration: Criar banco de dados ProducPlus
+-- Data: 10 de Novembro de 2025
+-- Descrição: Criação do banco de dados inicial para o sistema ProducPlus
+
+CREATE DATABASE IF NOT EXISTS ProducPlusDB;
+
+-- Usar o banco de dados ProducPlusDB
+USE ProducPlusDB;
---
+++ models/model.js
@@ -0,0 +1,3 @@
+// Esse arquivo foi criado apenas para
+// ilustrar a estrutura do projeto e será alterado
+// ou substituído conforme o desenvolvimento avançar.
