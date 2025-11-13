7ea65b5c32c9e336ed35fcdec51f92381df385ae
PHIETRO ALVES ALMEIDA <25170905@educ123.sp.senai.br>
2025-11-13 16:25:20 -0300

    commit 13_11_2025 Criação das tabelas do banco de dados

--- migrations/20251110_001_create_database.sql
+++ migrations/20251110_001_create_database.sql
@@ -1,4 +1,4 @@
---Migration: Criar banco de dados ProducPlus
+-- Migration: Criar banco de dados ProducPlus
 -- Data: 10 de Novembro de 2025
 -- Descrição: Criação do banco de dados inicial para o sistema ProducPlus
 
