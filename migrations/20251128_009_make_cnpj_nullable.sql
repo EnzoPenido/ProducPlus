d001cdafd0117264c179751200fbf7db0ca4b125
EnzoPenido <enzopoalencar01@gmail.com>
2025-11-28 14:13:55 -0300

    Migration consertada

--- migrations/20251128_009_make_cnpj_nullable.sql
+++ migrations/20251128_009_make_cnpj_nullable.sql
@@ -1,5 +1,6 @@
--- Migration: Alterar coluna cnpjFornecedor para permitir valores nulos
+-- Migration: Alterar coluna cnpjFornecedor para permitir valores nulos e produto adicionar coluna secao
 -- Data: 28 de novembro de 2025
--- Descrição: Esta migração altera a coluna cnpjFornecedor na tabela Produto para permitir valores nulos.
+-- Descrição: Esta migração altera a coluna cnpjFornecedor na tabela Produto para permitir valores nulos e adiciona uma nova coluna secao na tabela de produtos.
 
 ALTER TABLE Produto MODIFY cnpjFornecedor VARCHAR(14) NULL;
+ALTER TABLE Produto ADD secao VARCHAR(255) NULL;
