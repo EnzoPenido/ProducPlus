df09cca0d6338eb36a82e3f88636987106eff671
EnzoPenido <enzopoalencar01@gmail.com>
2025-12-04 22:31:48 -0300

    Edição de produto consertada e adicionado botão pra trocar foto do produto

--- routes/produtoRoutes.js
+++ routes/produtoRoutes.js
@@ -1,4 +1,7 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 import {
     listarProdutos,
     buscarProduto,
@@ -17,6 +20,37 @@ import { permitirRoles } from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
+// --- CONFIGURAÇÃO DO MULTER PARA PRODUTOS ---
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // Pega o ID da URL (ex: /produtos/7)
+        const id = req.params.id;
+        const uploadPath = path.join(process.cwd(), 'uploads', 'produtos', id);
+
+        // Cria a pasta se não existir
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        } else {
+            // SE A PASTA EXISTIR, APAGA A FOTO ANTIGA (Limpeza)
+            // Isso evita acumular lixo no servidor
+            const arquivos = fs.readdirSync(uploadPath);
+            for (const arquivo of arquivos) {
+                fs.unlinkSync(path.join(uploadPath, arquivo));
+            }
+        }
+
+        cb(null, uploadPath);
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now();
+        const nomeLimpo = file.originalname.replace(/\s+/g, '-');
+        cb(null, `perfil-${uniqueSuffix}-${nomeLimpo}`);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
 // --- ROTAS PÚBLICAS ---
 router.get("/", listarProdutos);
 router.get("/:id", buscarProduto);
@@ -30,7 +64,10 @@ router.put("/baixa-estoque", autenticarJWT, permitirRoles("CLIENTE", "ADMIN"), b
 
 // --- ROTAS DE ADMIN ---
 router.post("/", autenticarJWT, permitirRoles("ADMIN"), criarProduto);
-router.put("/:id", autenticarJWT, permitirRoles("ADMIN"), atualizarProduto);
+
+// ATENÇÃO: Adicionado upload.single('imagem') aqui 👇
+router.put("/:id", autenticarJWT, permitirRoles("ADMIN"), upload.single('imagem'), atualizarProduto);
+
 router.delete("/:id", autenticarJWT, permitirRoles("ADMIN"), excluirProduto);
 router.patch("/:id/estoque", autenticarJWT, permitirRoles("ADMIN"), ajustarEstoque);
 
